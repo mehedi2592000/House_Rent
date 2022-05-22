e53299b9a21716ce664170797cb2356cec0059be
mehedi <mehedi2592000@gmail.com>
2022-05-22 22:37:43 +0600

    tanant controller is done

---
+++ Home List Rent/Controllers/TenantController.cs
@@ -0,0 +1,52 @@
+﻿using Buseness_Entity_Layer;
+using Buseness_Logic_Layer.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Home_List_Rent.Controllers
+{
+    public class TenantController : ApiController
+    {
+        [Route("api/Tenant/Get")]
+        [HttpGet]
+        public HttpResponseMessage GetTenant()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK,TanantService.GetTanant());
+        }
+
+        [Route("api/Tanant/Getid/{id}")]
+        [HttpGet]
+        public HttpResponseMessage GetTanantid(int id)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, TanantService.GetTanantId(id));
+        }
+
+        [Route("api/Tanant/Add")]
+        [HttpPost]
+        public HttpResponseMessage AddTanant(TanantModel e)
+        {
+            bool data = TanantService.AddTanant(e);
+            return Request.CreateResponse(data ? HttpStatusCode.OK : HttpStatusCode.BadRequest);
+        }
+
+        [Route("api/Tanant/Edit")]
+        [HttpPost]
+        public HttpResponseMessage EditTanant(TanantModel e)
+        {
+            bool data=TanantService.EditTanant(e);
+            return Request.CreateResponse(data? HttpStatusCode.OK : HttpStatusCode.BadRequest);
+        }
+
+        [Route("api/Tanant/Delete/{id}")]
+        [HttpGet]
+        public HttpResponseMessage DeleteTanant(int id)
+        {
+            bool data = TanantService.DeleteTanant(id);
+            return Request.CreateResponse(data ? HttpStatusCode.OK : HttpStatusCode.BadRequest);
+        }
+    }
+}
